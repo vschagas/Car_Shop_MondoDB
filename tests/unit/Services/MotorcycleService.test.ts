92fdbad43ab5fe41f9b75e614a799cd067608d04
valmir <valmirchagas88@gmail.com>
2023-01-17 19:28:23 -0300

    test: add find by id motorcycleService test

--- tests/unit/Services/MotorcycleService.test.ts
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -10,6 +10,10 @@ const makeSut = () => {
 };
 
 describe('Motorcycle Service test', function () {
+  afterEach(function () {
+    Sinon.restore();
+  });
+
   it('Should create one motorcycle ', async function () {
     const motorcycleSerice = makeSut();
     Sinon.stub(Model, 'create').resolves(modelMotoResponse);
@@ -22,5 +26,12 @@ describe('Motorcycle Service test', function () {
     Sinon.stub(Model, 'find').resolves(arrayofMoto);
     const find = await motorcycleSerice.findAll();
     expect(find).to.be.deep.equal(arrayofMoto);
+  });
+
+  it('Should find by Id', async function () {
+    const motorcycleSerice = makeSut();
+    Sinon.stub(Model, 'findById').resolves(modelMotoResponse);
+    const find = await motorcycleSerice.findById('63c71aebf71d64670f2124ab');
+    expect(find).to.be.deep.equal(modelMotoResponse);
   });
 });
