ae705f94a385ebb2640653d835f0286f726eb4a8
valmir <valmirchagas88@gmail.com>
2023-01-17 17:37:59 -0300

    test: add mock files

---
+++ tests/unit/Mocks.ts
@@ -0,0 +1,35 @@
+export const createCarInput = {
+  // eslint-disable-next-line sonarjs/no-duplicate-string
+  model: 'Uno com Escada',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const createResponse = {
+  id: '63c6f8a4e18b8441d9b2086f',
+  model: 'Uno com Escada',
+  year: 2002,
+  color: 'Black',
+  buyValue: 15.99,
+  status: true,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const modelResponse = {
+  _id: {
+    $oid: '63c6f8a4e18b8441d9b2086f',
+  },
+  model: 'Uno com Escada',
+  year: 2002,
+  color: 'Black',
+  buyValue: 15.99,
+  status: true,
+  doorsQty: 4,
+  seatsQty: 5,
+  __v: 0,
+};
