94de085431baa62ca2f3bd975c7bcd8f8ea1350f
valmir <valmirchagas88@gmail.com>
2023-01-17 19:14:58 -0300

    refactor: move Motorservice to make sut method

--- tests/unit/Services/MotorcycleService.test.ts
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -4,9 +4,14 @@ import Sinon from 'sinon';
 import MotorService from '../../../src/Services/MotorService';
 import { createMotoInput, modelMotoResponse } from '../Mocks';
 
+const makeSut = () => {
+  const sut = new MotorService();
+  return sut;
+};
+
 describe('Motorcycle Service test', function () {
   it('Should create one motorcycle ', async function () {
-    const motorcycleSerice = new MotorService();
+    const motorcycleSerice = makeSut();
     Sinon.stub(Model, 'create').resolves(modelMotoResponse);
     const created = await motorcycleSerice.create(createMotoInput);
     expect(created).to.be.deep.equal(modelMotoResponse);
