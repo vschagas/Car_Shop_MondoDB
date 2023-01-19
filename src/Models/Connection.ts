a01fd2976914aaebc4aef6a20f3ed5a1f8237e25
valmir <valmirchagas88@gmail.com>
2023-01-19 17:00:04 -0300

    feat: add comunications with atlas MongoDb

--- src/Models/Connection.ts
+++ src/Models/Connection.ts
@@ -1,17 +1,11 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
+const MONGO_DB_URI = 'mongodb://localhost:7017/CarShop';
 
 const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI
-    || MONGO_DB_URL,
+  mongoDatabaseURI = process.env.MONGO_DB_URL
+    || MONGO_DB_URI,
 ) => mongoose.connect(mongoDatabaseURI);
 
 export default connectToDatabase;
-
-// const connectToDatabase = (
-//   mongoDatabaseURI = process.env.MONGO_URI,
-// ) => mongoose.connect(mongoDatabaseURI);
-
-// export default connectToDatabase;
