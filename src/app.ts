305cc05c3c54b6ef261c186d18bc8ab1aea01d95
valmir <valmirchagas88@gmail.com>
2023-01-18 23:57:43 -0300

    feat: add swagger doc inicial configuration

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 import routes from './Routes/Routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
